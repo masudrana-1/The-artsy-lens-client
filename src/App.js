5a91fad1d24ed41297c2fa44dbed5e39fbf5c44f
masudrana-1 <masudrana58855@gmail.com>
2022-11-08 16:45:31 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import AllServices from './components/AllServices/AllServices';
+import Blogs from './components/Blogs/Blogs';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Services from './components/Services/Services';
+import Main from './layout/Main';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Services></Services>
+        },
+        {
+          path: '/allservices',
+          element: <AllServices></AllServices>
+        },
+        {
+          path: '/servicedetails',
+          element: <AllServices></AllServices>
+        },
+        {
+          path: '/blogs',
+          element: <Blogs></Blogs>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        }
+      ]
+    }
+
+  ])
+
   return (
-    <div className="App">
-      header
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
